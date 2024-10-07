597aecd1b039daa0414ac616c7e7ad9e88aca5e2
Vaishali Jain <134711372+Vaishalijain10@users.noreply.github.com>
2024-10-08 00:34:40 +0530

    Update ContactMe.jsx
    API updated

--- frontend/src/components/ContactMe.jsx
+++ frontend/src/components/ContactMe.jsx
@@ -30,7 +30,8 @@ export default function ContactMe() {
 
     try {
       // Send form data to backend
-      const response = await axios.post("http://localhost:5000/send", formData);
+      const backendURL = "https://mern-portfolio-hnyv.vercel.app"; 
+      const response = await axios.post(`${backendURL}/send`, formData);
 
       if (response.status === 200) {
         // Display success toast notification
