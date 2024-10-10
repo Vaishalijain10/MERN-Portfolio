c8aa2012f60c7e5791df16e51b2dd36f7fa30d8c
Vaishalijain10 <vaishalijain.vj.10@gmail.com>
2024-10-11 00:40:10 +0530

    updates

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -3,7 +3,7 @@ import Cards from "./Cards";
 import Realtor from "../Images/Realtor.png";
 import LifeStyle from "../Images/LifeStyle.png";
 import DealsOnWheels from "../Images/dealsOnWheels.png";
-
+import LiveChatApp from "../Images/Live-Chat-App.png";
 export default function Projects() {
   return (
     <div className="bg-[#1e1e1e] w-[100%] ">
@@ -18,36 +18,45 @@ export default function Projects() {
         </h1>
 
         {/* Grid Layout */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ml-8 mr-8 text-justify">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0 ml-8 mr-8 text-justify min-h-0">
           <Cards
             image={Realtor}
             title="Realtor.com"
-            text="Realtor.com offers a comprehensive platform for buying, selling, and renting properties. With
-                    detailed listings, up-to-date information, photos, and virtual tours, it's easy to find your dream home or perfect
-                    rental. The user-friendly interface lets you filter by location, price, and more, ensuring you quickly find
-                    exactly what you need in the real estate market."
+            text="Talk-a-tive is a dynamic live chat app designed for seamless communication. 
+            With its user-friendly interface, you can easily connect with friends, family, or colleagues in real time. 
+            The app supports group chats and private chats, ensuring versatile interaction. 
+            Join Talk-a-tive today!"
+            // text="Realtor.com provides a comprehensive platform for buying, selling, and renting properties. With detailed listings, up-to-date information, and high-quality photos, discovering your dream home is easy. The user-friendly interface allows you to filter searches by location, price, and other criteria, ensuring you find exactly what you need in the real estate market quickly and efficiently."
             link1="https://realtor-clone-react-five-xi.vercel.app/"
             link2="https://github.com/Vaishalijain10/Realtor-Clone-React"
           />
           <Cards
-            image={LifeStyle}
-            title="LifeStyle"
-            text="LifeStyle is your destination for stunning imitation jewelry, offering a virtual store that brings elegance to your
-                    fingertips. Explore a wide range of beautifully crafted pieces, from timeless classics to trendy
-                    designs, all at affordable prices. Whether for a special occasion or everyday style, LifeStyle makes it easy to find
-                    the perfect jewelry from the comfort of your home."
-            link1="https://github.com/Vaishalijain10/LifeStyle"
-            link2="https://github.com/Vaishalijain10/LifeStyle"
+            image={LiveChatApp}
+            title="Live Chat App - Talk-A-Tive!"
+            text="Talk-a-tive is a dynamic live chat app designed for seamless communication. 
+            With its user-friendly interface, you can easily connect with friends, family, or colleagues in real time. 
+            The app supports group chats and private chats, ensuring versatile interaction. 
+            Join Talk-a-tive today!"
+            link1="https://talk-a-tive-kzf0.onrender.com/"
+            link2="https://github.com/Vaishalijain10/MERN-Live-Chat-App"
           />
           <Cards
             image={DealsOnWheels}
             title="Deals On Wheels"
-            text="Deals On Wheels offers a wide selection of cars from five top brands, making it your one-stop shop
-                    for finding the perfect vehicle. Whether you're looking for luxury, reliability, or affordability, our extensive
-                    inventory caters to every preference. Explore our user-friendly website to discover exclusive deals and drive away with
-                    your ideal car today."
+            text="Deals On Wheels is your ultimate destination for an extensive selection of cars from five top brands.  
+            Our one-stop shop makes it easy to find the perfect vehicle for your needs.    
+            Visit our user-friendly website to explore exclusive deals and drive away with your ideal car today!  "
             link1="https://deals-on-wheels.vercel.app/"
             link2="https://github.com/Vaishalijain10/deals_on_wheels"
+          />
+          <Cards
+            image={LifeStyle}
+            title="LifeStyle"
+            text="LifeStyle is your go-to destination for exquisite imitation jewelry.  
+                  Our virtual store offers a stunning collection, featuring everything from timeless classics to trendy designs.  
+                  All pieces are beautifully crafted and available at affordable prices, also makes it easy to find the perfect jewelry from the comfort of your home."
+            link1="https://github.com/Vaishalijain10/LifeStyle"
+            link2="https://github.com/Vaishalijain10/LifeStyle"
           />
         </div>
       </div>
